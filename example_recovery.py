d8be6bf3c601d007bca24ef37f10289ea6036329
Ziqi Zhu <kressety@163.com>
2023-11-05 21:46:27 +0000

    Refactor code and update example files

--- example_recovery.py
+++ example_recovery.py
@@ -1,9 +1,9 @@
 from os import remove
 from os.path import exists
-from os import system
+from shutil import copy2
 
 
 def example_recover():
     if exists('example.xlsx'):
         remove('example.xlsx')
-    system('wget https://src.mealuet.com/example.xlsx')
+    copy2('src/example.xlsx', 'example.xlsx')
